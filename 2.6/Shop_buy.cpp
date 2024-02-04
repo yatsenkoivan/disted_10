b632cfb4752375d35ce670a71b142f4b1b942b97
Ivan Yatsenko <aokiroyal16@gmail.com>
2024-02-04 20:43:09 +0200

    2.6 (reworked)

--- 2.6/Shop_buy.cpp
+++ 2.6/Shop_buy.cpp
@@ -3,91 +3,140 @@
 #include <map>
 using namespace std;
 
-#define uint unsigned int
-
 class Date
 {
-    private:
-        uint day;
-        uint month;
-        uint year;
-    public:
-        Date(uint day, uint month, uint year) : day{day}, month{month}, year{year} {}
+	private:
+		uint32_t day;
+		uint32_t month;
+		uint32_t year;
+	public:
+		Date(uint32_t d, uint32_t m, uint32_t y)
+			: day{d}, month{m}, year{y} {}
+
+		Date() : Date(1,1,1) {}
+
+		friend istream& operator>>(istream& in, Date& date)
+		{
+			in >> date.day >> date.month >> date.year;
+			return in;
+		}
+		
 };
 
 class Good
 {
-    private:
-        string name;
-        uint amount;
-        double price;
-        Date date;
-    public:
-        Good(string name, uint amount, double price, Date date) : name{name}, amount{amount}, price{price}, date{date} {}
-        Good() : Good("", 0, 0, Date(1,1,1)) {}
-        uint GetAmount() const
-        {
-            return amount;
-        }
-        double GetPrice() const
-        {
-            return price;
-        }
-        double Buy(uint amount_to_buy)
-        {
-            amount_to_buy = min(amount_to_buy, amount);
-            amount -= amount_to_buy;
-            return amount_to_buy * price;
-        }
+	private:
+		string name;
+		uint32_t amount;
+		double price;
+		Date date;
+	public:
+		Good(string name, uint32_t amount, double price, Date date)
+			: name{name}, amount{1}, price{1}, date{date} {}
+		Good() : Good{"", 1, 1, Date()} {}
+		
+		friend istream& operator>>(istream& in, Good& g)
+		{
+			in >> g.name >> g.amount >> g.price >> g.date;
+			
+			if (g.amount <= 0) g.amount = 1;
+			if (g.price <= 0) g.price = 1;
+			
+			return in;
+		}
+		
+		string getName() const
+		{
+			return name;
+		}
+		uint32_t getAmount() const
+		{
+			return amount;
+		}
+		void setAmount(uint32_t value)
+		{
+			amount = value;
+		}
+		double getPrice() const
+		{
+			return price;
+		}
+		
 };
+
+class Shop
+{
+	private:
+		map<string, Good> data;
+		double totalPrice;
+	public:
+		Shop() : totalPrice{0} {}
+		void operator+=(Good& g)
+		{
+			data[g.getName()] = g;
+			totalPrice += g.getAmount() * g.getPrice();
+		}
+		
+		double getTotalPrice() const
+		{
+			return totalPrice;
+		}
+		
+		double buy(string goodName, uint32_t amount)
+		{
+			map<string, Good>::iterator f = data.find(goodName);
+			if (f != data.end())
+			{
+				Good* g = &(f->second);
+				
+				amount = min(amount, g->getAmount());
+				g->setAmount(g->getAmount() - amount);
+				double total = amount * g->getPrice();
+				if (g->getAmount() == 0)
+				{
+					data.erase(f);
+				}
+				return total;
+			}
+			return 0;
+		}
+		
+};
+
 
 int main()
 {
-    ifstream input;
-    input.open("input_shop.txt", ios::in);
-    
-    int n;
-    input >> n;
-    
-    string name;
-    uint amount;
-    double price;
-    uint day, month;
-    uint year;
-    
-    map<string, Good> shop;
-    
-    for (int good_id=0; good_id<n; good_id++)
-    {
-        input >> name;
-        input >> amount;
-        input >> price;
-        input >> day >> month >> year;
-        
-        shop[name] = Good(name, amount, price, Date(day,month,year));
-    }
-    
-    input.close();
-    
-    input.open("input_buy.txt", ios::in);
-    
-    double total_price=0;
-    
-    input >> n;
-    for (int good_id=0; good_id<n; good_id++)
-    {
-        input >> name;
-        input >> amount;
-        
-        total_price += shop[name].Buy(amount);
-        
-    }
-    
-    input.close();
-    
-    ofstream output;
-    output.open("output_buy.txt", ios::out);
-    output << total_price << endl;
-    output.close();
-    
+	ifstream input;
+	input.open("input_shop.txt", ios::in);
+	
+	Shop shop;
+	
+	int goodsAmount;
+	input >> goodsAmount;
+	for (int goodId=0; goodId < goodsAmount; goodId++)
+	{
+		Good g;
+		input >> g;
+		shop += g;
+	}
+	input.close();
+	
+	input.open("input_buy.txt", ios::in);
+	
+	double totalPrice = 0;
+	input >> goodsAmount;
+	for (int goodId=0; goodId < goodsAmount; goodId++)
+	{
+		string name;
+		uint32_t amount;
+		input >> name >> amount;
+		totalPrice += shop.buy(name, amount);
+	}
+	input.close();
+	
+	ofstream output;
+	output.open("output_buy.txt", ios::out);
+	output << totalPrice << endl;
+	output.close();
+	
 }
--- 2.6/Shop_goods.cpp
+++ 2.6/Shop_goods.cpp
@@ -3,78 +3,108 @@
 #include <map>
 using namespace std;
 
-#define uint unsigned int
-
 class Date
 {
-    private:
-        uint day;
-        uint month;
-        uint year;
-    public:
-        Date(uint day, uint month, uint year) : day{day}, month{month}, year{year} {}
+	private:
+		uint32_t day;
+		uint32_t month;
+		uint32_t year;
+	public:
+		Date(uint32_t d, uint32_t m, uint32_t y)
+			: day{d}, month{m}, year{y} {}
+
+		Date() : Date(1,1,1) {}
+
+		friend istream& operator>>(istream& in, Date& date)
+		{
+			in >> date.day >> date.month >> date.year;
+			return in;
+		}
+		
 };
 
 class Good
 {
-    private:
-        string name;
-        uint amount;
-        double price;
-        Date date;
-    public:
-        Good(string name, uint amount, double price, Date date) : name{name}, amount{amount}, price{price}, date{date} {}
-        Good() : Good("", 0, 0, Date(1,1,1)) {}
-        uint GetAmount() const
-        {
-            return amount;
-        }
-        double GetPrice() const
-        {
-            return price;
-        }
+	private:
+		string name;
+		uint32_t amount;
+		double price;
+		Date date;
+	public:
+		Good(string name, uint32_t amount, double price, Date date)
+			: name{name}, amount{1}, price{1}, date{date} {}
+		Good() : Good{"", 1, 1, Date()} {}
+		
+		friend istream& operator>>(istream& in, Good& g)
+		{
+			in >> g.name >> g.amount >> g.price >> g.date;
+			
+			if (g.amount <= 0) g.amount = 1;
+			if (g.price <= 0) g.price = 1;
+			
+			return in;
+		}
+		
+		string getName() const
+		{
+			return name;
+		}
+		uint32_t getAmount() const
+		{
+			return amount;
+		}
+		double getPrice() const
+		{
+			return price;
+		}
+		
 };
+
+class Shop
+{
+	private:
+		map<string, Good> data;
+		double totalPrice;
+	public:
+		Shop() : totalPrice{0} {}
+		void operator+=(Good& g)
+		{
+			data[g.getName()] = g;
+			totalPrice += g.getAmount() * g.getPrice();
+		}
+		
+		double getTotalPrice() const
+		{
+			return totalPrice;
+		}
+		
+};
+
 
 int main()
 {
-    ifstream input;
-    input.open("input_shop.txt", ios::in);
-    
-    int n;
-    input >> n;
-    
-    string name;
-    uint amount;
-    double price;
-    uint day, month;
-    uint year;
-    
-    map<string, Good> shop;
-    
-    for (int good_id=0; good_id<n; good_id++)
-    {
-        input >> name;
-        input >> amount;
-        input >> price;
-        input >> day >> month >> year;
-        
-        shop[name] = Good(name, amount, price, Date(day,month,year));
-    }
-    
-    input.close();
-    
-    double total_price = 0;
-    
-    Good* good;
-    
-    for (pair<string, Good> good_pair : shop)
-    {
-        good = &(good_pair.second);
-        total_price += good->GetAmount() * good->GetPrice();
-    }
-    
-    ofstream output;
-    output.open("output_shop.txt", ios::out);
-    output << total_price << endl;
-    output.close();
+	ifstream input;
+	input.open("input_shop.txt", ios::in);
+	
+	Shop shop;
+	
+	int goodsAmount;
+	input >> goodsAmount;
+	
+	for (int goodId=0; goodId < goodsAmount; goodId++)
+	{
+		Good g;
+		input >> g;
+		shop += g;
+	}
+	
+	input.close();
+	
+	ofstream output;
+	output.open("output_shop.txt", ios::out);
+	
+	output << shop.getTotalPrice() << endl;
+	
+	output.close();
+	
 }
